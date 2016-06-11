507436831aade63f59a474881f08fbfeaf284bf9
zhongwu <zhongwuzw@qq.com>
2016-06-11 22:22:36 +0800

    Initial Commit

---
+++ VCInteractiveTransition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VCInteractiveTransition
+//
+//  Created by 钟武 on 16/6/11.
+//  Copyright © 2016年 钟武. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VCInteractiveTransition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VCInteractiveTransition
+//
+//  Created by 钟武 on 16/6/11.
+//  Copyright © 2016年 钟武. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
